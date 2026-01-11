0706467fa70712ebce9f4b0abc581fd4dbc13b57
Antti Eskelinen <antti.eskelinen.dev@gmail.com>
2026-01-11 11:36:50 +0200

    fix: avoid syncing theme on unrelevant file events

--- lua/omarchy-theme-loader/init.lua
+++ lua/omarchy-theme-loader/init.lua
@@ -1,8 +1,30 @@
 local opts = require("omarchy-theme-loader.default-opts")
 local omarchy_current_path = vim.fs.joinpath(vim.env.HOME, ".config", "omarchy", "current")
+local omarchy_current_theme_name_path = vim.fs.joinpath(omarchy_current_path, "theme.name")
+
+local is_omarchy = nil
+local is_legacy_omarchy = nil
 
 ---@type userdata|nil
 local handle
+
+local function check_is_omarchy()
+	if is_omarchy == nil then
+		is_omarchy = vim.uv.fs_stat(omarchy_current_path) ~= nil
+	end
+
+	return is_omarchy
+end
+
+---Check if running in Omarchy version prior 3.3., as 3.3. introduced a breaking change for the underlying theme mechanism.
+---@return boolean
+local function check_is_legacy_omarchy()
+	if is_legacy_omarchy == nil then
+		is_legacy_omarchy = vim.uv.fs_stat(omarchy_current_theme_name_path) == nil
+	end
+
+	return check_is_omarchy() and is_legacy_omarchy
+end
 
 --- Configuration for a single theme.
 ---@class Theme
@@ -15,39 +37,37 @@ local handle
 ---Get name of currently active Omarchy theme.
 ---@return string
 local function current_omarchy_theme_name()
-	-- Path to theme.name file
-	local theme_name_file = omarchy_current_path .. "/theme.name"
-
-	-- Read theme name from file
-	local file = io.open(theme_name_file, "r")
-	if file then
-		local theme = file:read("*l")
-		file:close()
-		if theme then
-			return theme
-		end
+	if check_is_legacy_omarchy() then
+		local symlink = omarchy_current_path .. "/theme"
+		local resolved = vim.fn.resolve(symlink)
+		return vim.fn.fnamemodify(resolved, ":t")
 	end
 
-	-- Fallback: try resolving symlink (legacy behavior)
-	local symlink = omarchy_current_path .. "/theme"
-	local resolved = vim.fn.resolve(symlink)
-	return vim.fn.fnamemodify(resolved, ":t")
-end
+	-- Read theme name from 'theme.name' file
+	local file = io.open(omarchy_current_theme_name_path, "r")
+	if not file then
+		error(string.format("Could not read current theme from '%s'", omarchy_current_theme_name_path))
+	end
 
-local function is_omarchy()
-	return vim.uv.fs_stat(omarchy_current_path) ~= nil
+	local theme = file:read()
+	file:close()
+	return theme
 end
 
 ---Sync Neovim theme to the current Omarchy theme.
 local function sync_theme()
-	local omarchy_theme = current_omarchy_theme_name()
+	local ok, omarchy_theme_result = pcall(current_omarchy_theme_name)
+	if not ok then
+		vim.notify(omarchy_theme_result, vim.log.levels.ERROR)
+		return
+	end
 
-	local theme = opts.themes[omarchy_theme]
+	local theme = opts.themes[omarchy_theme_result]
 	if not theme then
 		vim.notify(
 			string.format(
 				"Did not find Neovim theme for Omarchy theme '%s'. You can specify it via your omarchy-theme-loader configuration.",
-				omarchy_theme
+				omarchy_theme_result
 			),
 			vim.log.levels.ERROR
 		)
@@ -58,8 +78,7 @@ local function sync_theme()
 	vim.o.background = "dark"
 
 	-- Enable the actual theme.
-	local ok = pcall(vim.cmd.colorscheme, theme.colorscheme)
-	if not ok then
+	if not pcall(vim.cmd.colorscheme, theme.colorscheme) then
 		vim.notify(
 			string.format(
 				"Did not find colorscheme %s. You might need to install a Neovim plugin that specifies the colorscheme.",
@@ -86,7 +105,7 @@ end
 ---Do not call this directly from your Neovim config; Neovim will call it automatically on startup via
 ---`after/plugin/omarchy-theme-loader.lua`.
 M.start = function()
-	if not is_omarchy() then
+	if not check_is_omarchy() then
 		return
 	end
 
@@ -104,19 +123,28 @@ M.start = function()
 		return
 	end
 
-	-- Start listening for theme changes in ~/.config/omarchy/current folder.
-	vim.uv.fs_event_start(handle, omarchy_current_path, {}, function(_, filename, _)
-		-- We are not interested in other changes in current/ such as background changes.
-		-- Filter out theme changes (theme.name file or theme directory for legacy).
-		if filename ~= "theme.name" and filename ~= "theme" then
-			return
-		end
+	if check_is_legacy_omarchy() then
+		vim.uv.fs_event_start(handle, omarchy_current_path, {}, function(_, filename, _)
+			-- In older Omarchy versions we needed to listen for theme changes by watching the .config/omarchy/current folder,
+			-- and only react to updates in the 'theme' symlink within the folder.
+			if filename ~= "theme" then
+				return
+			end
 
-		-- vim.cmd.* commands must not be called in fast event context, so defer it to be invoked soon by the main event loop.
-		vim.schedule(function()
-			sync_theme()
+			-- vim.cmd.* commands must not be called in fast event context, so defer it to be invoked soon by the main event loop.
+			vim.schedule(function()
+				sync_theme()
+			end)
 		end)
-	end)
+	else
+		-- Watch 'theme.name' file and sync Neovim whenever there is an update.
+		vim.uv.fs_event_start(handle, omarchy_current_theme_name_path, {}, function(_, _, _)
+			-- vim.cmd.* commands must not be called in fast event context, so defer it to be invoked soon by the main event loop.
+			vim.schedule(function()
+				sync_theme()
+			end)
+		end)
+	end
 end
 
 return M
